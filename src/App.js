04c3b5a49a8dfaa994a0d8aa44fb9d8f7795abcf
Randy Arnold <33621459+RandyArnold@users.noreply.github.com>
2023-12-29 13:42:03 +0100

    feat: outcome determination (#14)
    Full working game, could be improved with animations and more advanced bot choices, good enouth for now

--- src/App.js
+++ src/App.js
@@ -25,14 +25,29 @@ function randomBotAction(): String
     return actions[Math.floor(Math.random()*actions.length)];
 }
 
-function Player({action = 'rock', score = 0, name = 'You', bot = false}) {
+function newScore(playerAction, botAction, playerScore, botScore)
+{
+    // key beats value
+    const gameLogic = {
+        'rock': 'scissors',
+        'scissors': 'paper',
+        'paper': 'rock'
+    };
+
+    if (gameLogic[playerAction] === botAction) playerScore++;
+    if (gameLogic[botAction] === playerAction) botScore++;
+
+    return {playerScore: playerScore, botScore: botScore};
+}
+
+function Player({action = 'rock', score, name = 'You', bot = false}) {
     return (
         <div className="Fightbox">
-            { bot &&  <span className="PlayerName">BOT {name}: 0</span> }
+            { bot &&  <span className="PlayerName">BOT {name}: {score}</span> }
             <div className="ActionIcon" id={bot ? 'botAction' : 'playerAction'}>
                 {icons[action]}
             </div>
-            { !bot &&  <span className="PlayerName">{name}: 0</span> }
+            { !bot &&  <span className="PlayerName">{name}: {score}</span> }
         </div>
     );
 }
@@ -46,12 +61,19 @@ function ActionButton({action, onActionSelected}) {
 }
 
 function App() {
-    const [playerAction, setPlayerAction] = useState("");
-    const [botAction, setBotAction] = useState("");
+    const [playerAction, setPlayerAction] = useState("rock");
+    const [botAction, setBotAction] = useState("rock");
+    const [playerScore, setPlayerScore] = useState(0);
+    const [botScore, setBotScore] = useState(0);
 
     const onActionSelected = (selectedAction) => {
         setPlayerAction(selectedAction);
-        setBotAction(randomBotAction());
+        const botAction = randomBotAction();
+        setBotAction(botAction);
+
+        const score = newScore(selectedAction, botAction, playerScore, botScore);
+        setPlayerScore(score.playerScore);
+        setBotScore(score.botScore);
     };
 
 
@@ -60,8 +82,8 @@ function App() {
     <div className="App">
       <div className="Container">
           <h1>ShiFuMi</h1>
-          <Player action={botAction || 'rock'} bot={true} name={botName}></Player>
-          <Player action={playerAction || 'rock'}></Player>
+          <Player action={botAction} score={botScore} bot={true} name={botName}></Player>
+          <Player action={playerAction} score={playerScore}></Player>
           <div className="Select">
               <ActionButton action={'rock'} onActionSelected={onActionSelected}></ActionButton>
               <ActionButton action={'paper'} onActionSelected={onActionSelected}></ActionButton>
